102f9bc0716b249a2a2defa6eddef78ce00a8b45
David Carlin <dave.carlin86@gmail.com>
2019-04-24 13:18:39 -0400

    liri added to portfolio

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -16,6 +16,7 @@ var projectList = [
 	"<div><a href='https://dscarlin.github.io/unit-4-game/' target='_blank'>Lord of the Rings RPG</a></div>",	
 	"<div><a href='https://dscarlin.github.io/TriviaGame/' target='_blank'>90's Tv Show Trivia </a></div>",
 	"<div><a href='https://dscarlin.github.io/unit-6-giphy/' target='_blank'>News and Entertainment Collection App</a></div>",
+	"<div><a href='https://github.com/dscarlin/cliWordGuess' target='_blank'>Node.js Word Guess Game</a></div>",
 	];
 		
 
